b0cc394901c04bfeeb6e2a8154798bbecf30deb1
Viola Elsenhans <elsenhans@ub.uni-leipzig.de>
2020-06-22 07:26:17 +0200

    UIFC-174-add-plugin-find-source-to-field-inferior-to add repeatable field for source

---
+++ src/components/MetadataSources/SourceTechnical/RepeatableFieldForSource.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field } from 'react-final-form';
+
+import {
+  Button,
+  Col,
+  IconButton,
+  Row,
+} from '@folio/stripes/components';
+
+import FindSource from './FindSourceForInferiorTo/FindSource';
+
+class RepeatableFieldForSource extends React.Component {
+  static propTypes = {
+    fields: PropTypes.object,
+  };
+
+  render() {
+    const { fields } = this.props;
+
+    return (
+      <Row>
+        <Col xs={12}>
+          {fields.map((elem, index) => (
+            <Row key={index}>
+              <Col xs={8}>
+                <Field
+                  ariaLabel={`inferiorTo #${parseInt(index + 1, 10)}`}
+                  name={elem}
+                  id={elem}
+                  component={FindSource}
+                  fullWidth
+                />
+              </Col>
+              <Col xs={1}>
+                <IconButton
+                  icon="trash"
+                  onClick={() => fields.remove(index)}
+                />
+              </Col>
+            </Row>
+          ))}
+        </Col>
+        <Col xs={4}>
+          <Button onClick={() => fields.push('')}>+ Add</Button>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default RepeatableFieldForSource;
